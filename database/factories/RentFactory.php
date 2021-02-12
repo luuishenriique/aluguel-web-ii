e1269797b3305d5ca3fac778d0884337b5bfb601
luuishenriique <luis_henrique_co@hotmail.com>
2021-02-12 19:16:20 -0300

    Adicionando factory de Rent

---
+++ database/factories/RentFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Rent;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class RentFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Rent::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'date_out' => $this->faker->dateTimeThisMonth(),
+            'date_in' => $this->faker->dateTimeThisMonth('+15 days'),
+        ];
+    }
+}
