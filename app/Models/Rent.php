d36c4e8759d539e3035217c2ca270f7b9bff5013
luuishenriique <luis_henrique_co@hotmail.com>
2021-02-12 00:12:48 -0300

    Adicionando relacionamento no model Rent

--- app/Models/Rent.php
+++ app/Models/Rent.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Rent extends Model
 {
     use HasFactory;
+
+    public class users()
+    {
+    	$this->belongsTo(User::class);
+    }
+
+    public class instruments()
+    {
+    	$this->belongsTo(Instrument::class);
+    }
+
+    public class orders()
+    {
+    	$this->belongsTo(Order::class);
+    }
 }
