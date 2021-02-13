161afbb697e290ed7e6a089516f7d478a73447c3
luuishenriique <luis_henrique_co@hotmail.com>
2021-02-12 21:20:23 -0300

    Listagem das entidades no lado n em relacionamento com o usuário (tabela mesclada entre rents e orders

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,17 @@
+<style type="text/css">
+
+    table{
+         border-collapse: collapse;
+         text-align: center;
+    }
+    table td{
+         border: 1px dotted black;
+    }
+    table th{
+        border: 1px dotted black;
+    }
+</style>
+
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -5,11 +19,45 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="py-12 flex items-stretch">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    <div class="block text-center textx1 pb-3">
+                        <b>Histórico de Aluguéis</b>
+                        <br>
+                        <br>
+                        @php
+                        $rents = App\Models\Rent::where('client_id', Auth::user()->id)->get();
+                        @endphp
+                        <table class="table-auto">
+                            <thead>
+                                <tr class="bg-emerald-200">
+                                    <th>Nº Protocolo</th>
+                                    <th>Data de aluguel</th>
+                                    <th>Data de devolução</th>
+                                    <th>Cod. Instrumento</th>
+                                    <th>Valor($)</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($rents as $rent)
+                            @php
+                            $orders = App\Models\Order::where('rent_id', $rent->id)->get();
+                            @endphp
+                            <tr>
+                            <td>{{$rent->id}}</td>
+                            <td>{{$rent->date_out}}</td>
+                            <td>{{$rent->date_in}}</td>
+                            <td>{{$rent->instrument_id}}</td>
+                            @foreach($orders as $order)
+                            <td>{{$order->total}}</td>
+                            @endforeach
+                            </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
